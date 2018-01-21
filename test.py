4db9251646c8ac367febbc0f6f23cc7b8e3b0e76
Naji Alamrony <nalamron@uwaterloo.ca>
2018-01-20 22:01:51 -0500

    test.py added

---
+++ test.py
@@ -0,0 +1,18 @@
+from jinja2 import *
+
+def show_all_attrs(value):
+    res = []
+    for k in dir(value):
+        res.append('%r %r\n' % (k, getattr(value, k)))
+    return '\n'.join(res)
+
+env = Environment()
+env.filters['show_all_attrs'] = show_all_attrs
+
+# using the filter
+tmpl = env.from_string('''{{v|show_all_attrs}}''')
+class Myobj(object):
+    a = 1
+    b = 2
+
+print (tmpl.render(v=Myobj()))
